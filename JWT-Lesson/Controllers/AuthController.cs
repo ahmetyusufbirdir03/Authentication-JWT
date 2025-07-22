0fa41c3ecd397fc9d597e06e246aeeecd4f2bc4b
ahmetyusufbirdir03 <168556979+ahmetyusufbirdir03@users.noreply.github.com>
2025-07-22 16:14:36 +0300

    Register ve Login endpointleri oluşturuldu ve JWT Token oluşturuldu

---
+++ JWT-Lesson/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using JWT_Lesson.Entities;
+using JWT_Lesson.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWT_Lesson.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController(IConfiguration configuration) : ControllerBase
+    {
+        public static User user = new();
+
+        [HttpPost]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("user not found!");
+            }
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("wrong password!");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user )
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience : configuration.GetValue<string>("AppSettings:Audience"),
+                claims : claims,
+                expires : DateTime.UtcNow.AddDays(1),
+                signingCredentials : credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+    }
+}
